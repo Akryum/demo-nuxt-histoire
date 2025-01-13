77c23c29431c1bc33f7e977117af0e05de2689e4
Guillaume Chau <guillaume.b.chau@gmail.com>
2025-01-13 18:15:45 +0100

    feat: app + layer + histoire

---
+++ packages/layer-ui/components/Test.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <h1 class="poppins" style="font-size: 2rem;">
+    Hello world with font
+  </h1>
+  <h1 class="not-poppins" style="font-size: 2rem;">
+    Hello world without font
+  </h1>
+</template>
---
+++ packages/layer-ui/stories/Test.story.vue
@@ -0,0 +1,5 @@
+<template>
+  <Story>
+    <Test />
+  </Story>
+</template>
