20e9314fd92e85d7d0dd7755afe1742bfd607c26
Guillaume Chau <guillaume.b.chau@gmail.com>
2025-01-13 18:20:58 +0100

    chore: remove vite base override

--- packages/layer-ui/histoire.config.ts
+++ packages/layer-ui/histoire.config.ts
@@ -5,9 +5,4 @@ import { HstNuxt } from '@histoire/plugin-nuxt'
 export default defineConfig({
   plugins: [HstVue(), HstNuxt()],
   setupFile: 'histoire.setup.ts',
-  vite: {
-    // publicDir: 'public',
-    // base: '/_nuxt/',
-    base: '/',
-  },
 })
--- packages/layer-ui/nuxt.config.ts
+++ packages/layer-ui/nuxt.config.ts
@@ -6,4 +6,6 @@ export default defineNuxtConfig({
   css: [
     resolve('./styles/fonts.css'),
   ],
+
+  compatibilityDate: '2025-01-13',
 })
